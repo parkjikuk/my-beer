bce2bbe911e7e75ae90e8e61d53de85bf4ad6799
parkjikuk <paxkk@naver.com>
2023-01-17 04:37:37 +0900

    feat : router pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Detail from './pages/Detail';
+import Home from './pages/Home';
+import Like from './pages/Like';
+import Profile from './pages/Profile';
+import Search from './pages/Search';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <h1>본문</h1>
-    </div>
+    <>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/like" element={<Like />} />
+        <Route path="/detail/:id" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
