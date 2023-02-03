35b50a9e04f4bfa0b28ebe5c224a9bc1a201a5de
parkjikuk <paxkk@naver.com>
2023-02-03 23:29:58 +0900

    add signup features

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,16 +1,51 @@
 import styled from 'styled-components';
 import { panelActive } from '../pages/Login';
+import { useAppDispatch } from '../store/store';
 import { Btn, Form, FormTitle } from './Signin';
+import { useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { firebaseAuth } from '../firebase/firebase';
+
+interface User {
+  email: string;
+  password: string;
+  name: string;
+}
 
 function Signup({panelActive}: panelActive) {
+  const dispatch = useAppDispatch();
+  const [formValues, setFormValues] = useState<User>({
+    email: "",
+    password: "",
+    name: "",
+  })
+
+
+  const handleSignUp = async() => {
+    // if(!formValues.name) {
+    //   return alert('닉네임을 입력하세요');
+    // }
+    try {
+      const { email, password } = formValues;
+      await createUserWithEmailAndPassword(firebaseAuth,email,password)
+      .then((userAuth) => {
+        console.log(userAuth);
+      })
+    } catch (err) {
+      console.log(err);
+      
+    }
+  }
+
   return (
     <Container panelActive={panelActive}>
       <Form>
         <FormTitle>회원가입</FormTitle>
 
-        <SignupInput placeholder='이메일'/>
-        <SignupInput placeholder='비밀번호'/>
-        <Btn>회원가입</Btn>
+        <SignupInput placeholder='닉네임' type='text' name='name' value={formValues.name} onChange={(e) => setFormValues({...formValues, [e.target.name]: e.target.value})} />
+        <SignupInput placeholder='이메일' type='email' name='email' value={formValues.email} onChange={(e) => setFormValues({...formValues, [e.target.name]: e.target.value})}/>
+        <SignupInput placeholder='비밀번호' type='password' name='password' value={formValues.password} onChange={(e) => setFormValues({...formValues, [e.target.name]: e.target.value})}/>
+        <Btn onClick={handleSignUp}>회원가입</Btn>
       </Form>
 
     </Container>
