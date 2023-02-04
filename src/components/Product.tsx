de44f8fece023c440769e6cc7ab7b65f05fdf0d4
parkjikuk <paxkk@naver.com>
2023-02-05 04:37:23 +0900

    get beer date dynamically

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -2,69 +2,63 @@ import React from 'react';
 import styled from 'styled-components';
 import { BsHeart, BsHeartFill } from 'react-icons/bs';
 import { Link } from "react-router-dom";
+import { ProductItems } from '../store/features/productSlice';
 
-function Product() {
+function Product({data} : {data: ProductItems}) {
   return (
     <Container>
-      <ProductItem>
-        <Link to={`/detail`}>
-          <ProductImg src="https://i.ibb.co/jD6gQ9c/01-26934467-1-removebg-preview.png" alt="product" />
-        </Link>
+        <ProductItem to={`/detail`}>
+          <ProductImg src={data.image}/>
+        </ProductItem>
         <ProductInfo>
-          <ProductTitle>테라</ProductTitle>
+          <ProductTitle>{data.name}</ProductTitle>
           <ProductLike>
             <BsHeart />
-            <BsHeartFill />
+            {/* <BsHeartFill /> */}
           </ProductLike>
         </ProductInfo>   
-      </ProductItem>
-
-
-
-      <ProductItem>제품2</ProductItem>
-      <ProductItem>제품3</ProductItem>
     </Container>
   );
 }
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   flex-wrap: wrap;
   margin-top: 30px;
   justify-content: space-evenly;
+  width: 50%;
 `;
 
-const ProductItem = styled.div`
-  width: 27%;
+const ProductItem = styled(Link)`
   background-color: #FFE4E1;
-  height: 300px;
+  height: 200px;
   border-radius: 10px;
-  padding: 10px;
-  margin-bottom: 30px;
+  padding: 20px;
   text-align: center;
   &:hover {
     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   }
+  text-decoration: none;
 `;
 
 const ProductImg = styled.img`
   max-height: 150px;
-  width: 100%
   object-fit: contain;
 `;
 
 const ProductInfo = styled.div`
   display: flex;
-  font-size: 25px;
+  font-size: 20px;
   background-color: white;
   border-radius: 10px;
   justify-content: space-between;
   padding: 20px;
-  margin-top: 50px;
 `;
 
 const ProductTitle = styled.div`
   font-weight: 700;
+  color: black;
 `;
 
 const ProductLike = styled.div`
