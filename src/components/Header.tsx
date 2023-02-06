65bf7f140c99013434c28d6130e95281ac3a9855
parkjikuk <paxkk@naver.com>
2023-02-06 23:35:44 +0900

    Change UI according to user authentication status

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { firebaseAuth } from '../firebase/firebase';
+import { useAppDispatch, useAppSeletor } from '../store/store';
+import { authSlice } from '../store/features/authSlice';
 
 function Header() {
+  const dispatch = useAppDispatch();
+  const isLoggedIn = useAppSeletor((state) => state.auth.authenticated);
+  const [displayName, setDisplayName] = useState<string | null>("");
+
+  const logoutHanlder = () => {
+    signOut(firebaseAuth).then(() => {
+      alert('로그아웃 성공');
+    }).catch((error) => {
+      alert(error.message);
+    })
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(firebaseAuth, (user) => {
+      if(user) {
+        const uid = user.uid;
+        setDisplayName(user.displayName);
+        console.log(displayName);
+
+        dispatch(authSlice.actions.login({
+          email: user.email,
+          userName: user.displayName,
+          userID: user.uid,
+        }))
+      }  else {
+        setDisplayName("");
+        dispatch(authSlice.actions.logout());
+      }
+    })
+  }, [dispatch, displayName]);
+
   return (
     <Block>
       <Link to={`/`}>
@@ -11,9 +46,14 @@ function Header() {
       <SearchIput type="text" />
 
       <NavList>
+        {isLoggedIn ? 
+        <>
         <NavItem as="a" href="/like">찜 맥주</NavItem>
-        <NavItem as="a" href="/">프로필</NavItem>
-        <NavItem as="a" href="/">로그인</NavItem>
+        <NavItem as="a" href="/profile">프로필</NavItem>
+        <NavItem as="a" href="/login" onClick={logoutHanlder}>로그아웃</NavItem> 
+        <UserName>{displayName} 님!</UserName>
+        </>
+        : <NavItem as="a" href="/login">로그인</NavItem>}
       </NavList>
     </Block>
   );
@@ -27,7 +67,7 @@ const Block = styled.header`
   background-color: #FFE4E1;
   position: sticky;
   top: 0;
-  z-index: 10;
+  z-index: 1;
   justify-content: space-evenly;
 `;
 
@@ -64,6 +104,14 @@ const NavItem = styled.li`
   &:hover {
     color: white;
   }
+`;
+
+const UserName = styled.div`
+font-size: 20px;
+padding: 8px;
+background-color: white;
+border-radius: 10px;
+font-weight: 700;
 `;
 
 
