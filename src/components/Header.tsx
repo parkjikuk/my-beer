044c6b7253227c407ff22f946dde1a0b5ddb4bcf
parkjikuk <paxkk@naver.com>
2023-02-11 05:25:13 +0900

    add  user data on local storage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,42 +1,64 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { firebaseAuth } from '../firebase/firebase';
 import { useAppDispatch, useAppSeletor } from '../store/store';
 import { authSlice } from '../store/features/authSlice';
+import { toast } from 'react-toastify';
+
 
 function Header() {
   const dispatch = useAppDispatch();
   const isLoggedIn = useAppSeletor((state) => state.auth.authenticated);
-  const [displayName, setDisplayName] = useState<string | null>("");
-
-  const logoutHanlder = () => {
-    signOut(firebaseAuth).then(() => {
-      alert('로그아웃 성공');
-    }).catch((error) => {
-      alert(error.message);
-    })
-  };
+  const [displayName, setDisplayName] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    onAuthStateChanged(firebaseAuth, (user) => {
-      if(user) {
-        const uid = user.uid;
+    const unsubscribe = firebaseAuth.onAuthStateChanged((user) => {
+      if (user) {
         setDisplayName(user.displayName);
-        console.log(displayName);
-
+        localStorage.setItem('loggedInUser', JSON.stringify(user));
         dispatch(authSlice.actions.login({
           email: user.email,
           userName: user.displayName,
           userID: user.uid,
-        }))
-      }  else {
-        setDisplayName("");
+        }));
+      } else {
+        localStorage.removeItem('loggedInUser');
+        setDisplayName(null);
         dispatch(authSlice.actions.logout());
       }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [dispatch]);
+
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('loggedInUser');
+    if (storedUser) {
+      const user = JSON.parse(storedUser);
+      console.log(user)
+      setDisplayName(user.displayName);
+      dispatch(authSlice.actions.login({
+        email: user.email,
+        userName: user.displayName,
+        userID: user.uid,
+      }));
+    }
+  }, [dispatch]);
+
+  const logoutHanlder = () => {
+    signOut(firebaseAuth).then(() => {
+      toast.success("로그아웃 하셨습니다.");
+      navigate("/");
+    }).catch((error) => {
+      toast.error(error.message)
     })
-  }, [dispatch, displayName]);
+  };
+
 
   return (
     <Block>
@@ -44,7 +66,6 @@ function Header() {
         <Logo src="https://i.ibb.co/nQCJHJq/Free-Sample-By-Wix-11zon-removebg-preview.png" alt="logo" />
       </Link>
       <SearchIput type="text" />
-
       <NavList>
         {isLoggedIn ? 
         <>
