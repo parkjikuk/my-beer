e3078fea298b7a4b884123b9705fd142f9bb5941
parkjikuk <paxkk@naver.com>
2023-02-12 23:55:02 +0900

    add  SignUp validation and refactor code

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,51 +1,113 @@
 import styled from 'styled-components';
 import { panelActive } from '../pages/Login';
-import { useAppDispatch } from '../store/store';
 import { Btn, Form, FormTitle } from './Signin';
-import { useState } from 'react';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { firebaseAuth } from '../firebase/firebase';
+import FormInput from './FormInput';
+import { toast } from 'react-toastify';
 
-interface User {
+
+export interface IInput {
   email: string;
   password: string;
   name: string;
+  confirmPassword: string;
+}
+
+export interface IProps {
+  value: string;
+  id: number;
+  name: string;
+  type: string;
+  placeholder: string;
+  errorMessage?: string;
+  pattern?: string;
+  required?: boolean;
 }
 
 function Signup({panelActive}: panelActive) {
-  const dispatch = useAppDispatch();
-  const [formValues, setFormValues] = useState<User>({
+  const [formValues, setFormValues] = useState<IInput>({
     email: "",
     password: "",
     name: "",
-  })
+    confirmPassword: "",
+  });
 
-
-  const handleSignUp = async() => {
-    // if(!formValues.name) {
-    //   return alert('닉네임을 입력하세요');
-    // }
-    try {
-      const { email, password } = formValues;
-      await createUserWithEmailAndPassword(firebaseAuth,email,password)
-      .then((userAuth) => {
-        console.log(userAuth);
-      })
-    } catch (err) {
-      console.log(err);
-      
+  const inputs = [
+    {
+      id: 1,
+      name: "name",
+      type: "text",
+      placeholder: "닉네임",
+      errorMessage: "닉네임은 3글자 이상 6글자 이하여야 합니다.",
+      pattern: "^[A-Za-z0-9ㄱ-ㅎ가-힣]{3,6}",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "이메일",
+      errorMessage: "올바른 이메일 형식이 아닙니다.",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "password",
+      type: "password",
+      placeholder: "비밀번호",
+      errorMessage: "비밀번호는 문자와 숫자가 포함되어야하며 8자 이상이여야 합니다.",
+      pattern: `^.*(?=.{8,})(?=.*[0-9])(?=.*[a-zA-Z]).*$`,
+      required: true,
+    },
+    {
+      id: 4,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "비밀번호 확인",
+      errorMessage: "비밀번호가 일치하지 않습니다.",
+      required: true,
+      pattern: formValues.password,
     }
+  ]
+
+
+  const handleSignUp = async(e: React.MouseEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    
+      const { email, password, name } = formValues;
+      await createUserWithEmailAndPassword(firebaseAuth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        updateProfile(user, {
+          displayName: name,
+        })
+        firebaseAuth.signOut();
+        toast.success('회원가입에 성공하셨습니다.')
+        window.location.replace("/login");
+        console.log(user);
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      })
   }
 
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValues({...formValues, [e.target.name]: e.target.value});
+  }
+
+  console.log(formValues);
   return (
     <Container panelActive={panelActive}>
-      <Form>
+      <Form onSubmit={handleSignUp}>
         <FormTitle>회원가입</FormTitle>
 
-        <SignupInput placeholder='닉네임' type='text' name='name' value={formValues.name} onChange={(e) => setFormValues({...formValues, [e.target.name]: e.target.value})} />
-        <SignupInput placeholder='이메일' type='email' name='email' value={formValues.email} onChange={(e) => setFormValues({...formValues, [e.target.name]: e.target.value})}/>
-        <SignupInput placeholder='비밀번호' type='password' name='password' value={formValues.password} onChange={(e) => setFormValues({...formValues, [e.target.name]: e.target.value})}/>
-        <Btn onClick={handleSignUp}>회원가입</Btn>
+        {inputs.map((input) => (
+          <FormInput key={input.id} {...input} value={formValues[input.name as keyof IInput]} onChange={onChange} />
+        ))}
+        <Btn type='submit'>회원가입</Btn>
       </Form>
 
     </Container>
@@ -63,15 +125,6 @@ transition: all 0.6s ease-in-out;
 transform: ${props=> props.panelActive ? 'translateX(100%)' : null };
 opacity: ${props => props.panelActive ? '1' : '0'};
 z-index: ${props => props.panelActive ? '5' : '1'};
-`;
-
-const SignupInput = styled.input`
-background-color: #fff;
-border: none;
-padding: 20px;
-padding: 0.9rem 0.9rem;
-margin: 15px 0;
-width: 100%;
 `;
 
 
