2178c4851fafa9456e62beea3f370dfeda376725
parkjikuk <paxkk@naver.com>
2023-02-17 23:58:24 +0900

    feat : HTTP get request and type RootState

--- src/store/features/likeSlice.ts
+++ src/store/features/likeSlice.ts
@@ -1,5 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { PayloadAction } from "@reduxjs/toolkit/dist/createAction";
+import { RootState } from './../store';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from 'axios';
 
 export interface Like{
   id: number;
@@ -15,19 +16,25 @@ const initialState: LikeState = {
   likeItems: [],
 }
 
+export const getUserLikedBeers = createAsyncThunk<Like[], void, { state: RootState }>(
+  "beer/getLiked",
+  async (_, thunkAPI) => {
+    const email = thunkAPI.getState().auth.email;
+    const { data: { beers } } = await axios.get(`http://localhost:5000/api/user/liked/${email}`);
+    console.log(beers)
+    return beers;
+  }
+);
+
 export const LikeSlice = createSlice({
   name: "like",
   initialState,
-  reducers: {
-    addItem:(state, action: PayloadAction<{name: string, image: string}> ) => {
-      state.likeItems.push({
-        id: state.likeItems.length,
-        name: action.payload.name,
-        image: action.payload.image
-      });
-    }
-  }
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getUserLikedBeers.fulfilled, (state, action) => {
+      state.likeItems = action.payload;
+    })
+  },
 });
 
-export default LikeSlice.reducer;
-export const { addItem } = LikeSlice.actions;
+export default LikeSlice;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import productSlice from './features/productSlice';
 import { authSlice } from './features/authSlice';
+import likeSlice from './features/likeSlice';
+
+export type RootState = ReturnType<typeof store.getState>;
 
 const store = configureStore({
   reducer: {
     beer: productSlice.reducer,
     auth: authSlice.reducer,
+    like: likeSlice.reducer,
   }
 })
 
 export const useAppDispatch: () => typeof store.dispatch = useDispatch;
-export const useAppSeletor: TypedUseSelectorHook<ReturnType<typeof store.getState>> = useSelector;
+export const useAppSelector: TypedUseSelectorHook<ReturnType<typeof store.getState>> = useSelector;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
 
 export default store;
