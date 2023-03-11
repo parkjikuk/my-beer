1dcf7a11f7c24bf4cb9ae55291e06049e158e004
parkjikuk <paxkk@naver.com>
2023-03-12 05:20:41 +0900

    fix Loading rendering issue from Like to Detail page

--- src/pages/Like.tsx
+++ src/pages/Like.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Loading from '../components/Loading';
 import Product from '../components/Product';
 import { getUserLikedBeers } from '../store/features/likeSlice';
+import { fetchData } from '../store/features/productSlice';
 import { useAppDispatch, useAppSelector } from '../store/store';
 
 function Like() {
@@ -12,10 +13,11 @@ function Like() {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
+    dispatch(fetchData());
+
     if (email) {
       dispatch(getUserLikedBeers());
     }
-
   }, [dispatch, email]);
 
   return (
@@ -26,8 +28,8 @@ function Like() {
         <Block>
           <LikeTitle>{userName}님의 맥주함</LikeTitle>
           <Container>
-          {userLikeBeers.map((beer) => {
-            return <Product data={beer} isLiked={true}/>
+          {userLikeBeers?.map((beer) => {
+            return <Product key={beer.id} data={beer} isLiked={true}/>
           })}
           </Container>
         </Block>
@@ -46,13 +48,14 @@ place-items: center;
 grid-template-columns: 2fr 2fr 2fr 2fr;
 grid-template-rows: 3fr 3fr 3fr;
 width: 100%;
-height: 100%;
+min-height: 100vh;
+background-color: #412728;
 `;
 
 const LikeTitle = styled.h2`
 display: inline-block;
 padding: 0.5em 1em;
-background-color: #B87C4C;
+background-color: #b87c4c;
 color: #fff;
 width: fit-content;
 margin: 20px 6%;
