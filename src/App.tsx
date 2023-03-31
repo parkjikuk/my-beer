d30038030c21e9a6589354a2d0ca51205c3f7ddc
parkjikuk <paxkk@naver.com>
2023-04-01 04:16:37 +0900

    add automatic logout on browser window close

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Detail from './pages/Detail';
@@ -7,8 +7,24 @@ import Like from './pages/Like';
 import Login from './pages/Login';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useAppDispatch } from './store/store';
+import { authSlice } from './store/features/authSlice';
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      dispatch(authSlice.actions.logout());
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [dispatch]);
+
   return (
     <>
     <BrowserRouter>
